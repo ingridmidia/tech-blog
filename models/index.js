d2dc000aa84190ef26a9f98ee9d83c4ae7601be9
ingridmidia <ingridmidia@gmail.com>
2023-10-29 11:32:19 -0500

    added seed data

--- models/index.js
+++ models/index.js
@@ -18,4 +18,12 @@ Comment.belongsTo(User, {
     foreignKey: "user_id"
 });
 
+Comment.belongsTo(Post, {
+    foreignKey: "post_id"
+});
+
+Post.hasMany(Comment, {
+    foreignKey: "post_id"
+});
+
 module.exports = { User, Post, Comment };
---
+++ seeds/index.js
@@ -0,0 +1,18 @@
+const sequelize = require('../config/connection');
+const { User, Post, Comment } = require('../models');
+
+const userSeedData = require('./userSeedData.json');
+const postSeedData = require('./postSeedData.json');
+const commentSeedData = require('./commentSeedData.json');
+
+const seedDatabase = async () => {
+    await sequelize.sync({ force: true });
+
+    const users = await User.bulkCreate(userSeedData);
+    const post = await Post.bulkCreate(postSeedData);
+    const comments = await Comment.bulkCreate(commentSeedData);
+
+    process.exit(0);
+};
+
+seedDatabase();
