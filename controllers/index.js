14b9b38a58086296ad5f4353595b3e90428c115f
ingridmidia <ingridmidia@gmail.com>
2023-10-29 14:34:46 -0500

    added route to show one post with its comments

--- controllers/index.js
+++ controllers/index.js
@@ -1,14 +1,32 @@
 const router = require('express').Router();
-const { User, Post } = require("../models");
+const { User, Post, Comment } = require("../models");
 
 router.get('/', async (req, res) => {
   try {
     const postsData = await Post.findAll({ include: [{ model: User }] });
     const posts = postsData.map(post => {
       return post.get({ plain: true })
-    })
-    console.log(posts);
+    });
     res.render('home', { posts });
+  } catch (err) {
+    res.status(500).json(err);
+  };
+});
+
+router.get("/post/:id", async (req, res) => {
+  try {
+    const postData = await Post.findByPk(req.params.id, { include: [{ model: User }] });
+    const post = postData.get({ plain: true });
+    const commentsData = await Comment.findAll({
+      where: {
+        post_id: req.params.id
+      },
+      include: [{ model: User }]
+    });
+    const comments = commentsData.map(comment => {
+      return comment.get({ plain: true })
+    });
+    res.render('post', { post, comments });
   } catch (err) {
     res.status(500).json(err);
   };
