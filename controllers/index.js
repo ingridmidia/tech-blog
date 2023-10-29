a06ce4f63da41af381ac2f0dc4d09eb908ea66d6
ingridmidia <ingridmidia@gmail.com>
2023-10-29 13:48:29 -0500

    render all posts on home page

--- controllers/index.js
+++ controllers/index.js
@@ -1,7 +1,17 @@
 const router = require('express').Router();
+const { User, Post } = require("../models");
 
 router.get('/', async (req, res) => {
-  res.render('home');
+  try {
+    const postsData = await Post.findAll({ include: [{ model: User }] });
+    const posts = postsData.map(post => {
+      return post.get({ plain: true })
+    })
+    console.log(posts);
+    res.render('home', { posts });
+  } catch (err) {
+    res.status(500).json(err);
+  };
 });
 
 module.exports = router;
