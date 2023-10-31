dff522d2ef19c86ef86cebf1440522ca7aa047a3
ingridmidia <ingridmidia@gmail.com>
2023-10-31 15:35:50 -0500

    finished readme

--- README.md
+++ README.md
@@ -1,1 +1,8 @@
-# tech-blog
+# Tech Blog
+
+## Description
+This is a CMS-style blog, where developers can publish their blog posts and comment on other developers’ posts as well. This application uses Handlebars.js as the templating language, Sequelize as the ORM, and the express-session npm package for authentication. Bootstrap is used in the blog design.
+
+## [Deployed Website](https://ingrid-tech-blog-5ff7d16ff03b.herokuapp.com/)
+
+![Alt text](./public/assets/tech-blog.png)
--- controllers/api/user-routes.js
+++ controllers/api/user-routes.js
@@ -21,7 +21,6 @@ router.post('/', async (req, res) => {
 router.post('/login', async (req, res) => {
   try {
     const userData = await User.findOne({ where: { username: req.body.username } });
-    console.log(userData);
 
     if (!userData) {
       res.status(400).json({ message: 'Incorrect username or password, please try again' });
@@ -29,7 +28,6 @@ router.post('/login', async (req, res) => {
     }
 
     const validPassword = await userData.checkPassword(req.body.password);
-    console.log(validPassword);
 
     if (!validPassword) {
       res.status(400).json({ message: 'Incorrect username or password, please try again' });
